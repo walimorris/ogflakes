8cc95786f36b5282f432ed694362c3e3d5169983
Wali Morris <walimmorris@gmail.com>
2022-02-02 01:45:56 -0800

    refactor OgCerealController functions to OgCerealService for best practice

--- src/main/java/com/morris/ogflakes/controller/OgCerealController.java
+++ src/main/java/com/morris/ogflakes/controller/OgCerealController.java
@@ -3,7 +3,6 @@ package com.morris.ogflakes.controller;
 import com.morris.ogflakes.model.OgCereal;
 import com.morris.ogflakes.repository.OgCerealRepository;
 import com.morris.ogflakes.service.OgCerealService;
-import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,15 +13,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/ogcereal")
 public class OgCerealController {
     private static final Logger logger = LoggerFactory.getLogger(OgCerealController.class);
+
+    private static final String RESULTS = "results";
+    private static final String EMPTY = "empty";
+    private static final String OG_FLAKES_LIST = "ogFlakesList";
+    private static final String MESSAGE_1 = "message1";
+    private static final String MESSAGE_2 = "message2";
 
     private final OgCerealRepository ogCerealRepository;
 
@@ -38,58 +43,29 @@ public class OgCerealController {
         return "contribute";
     }
 
-
     @GetMapping("/showcase")
     public String getOgCereal(@RequestParam(value = "q", required = false) String query, Model model,
                               HttpServletRequest request) {
         List<OgCereal> maxQueryResultsList;
         if (StringUtils.isNotEmpty(query)) {
-
-            // search both upper and lowercase query with regex and return greatest results list
-            if (StringUtils.isAllLowerCase(query.substring(0,1))) {
-                query = StringUtils.join(query.substring(0,1).toUpperCase(), query.substring(1));
+            maxQueryResultsList = ogCerealService.getCaseOptimizedResultsList(query);
+            if (ogCerealService.isEmptyOrNonValidatedResults(maxQueryResultsList)) {
+                model.addAttribute(RESULTS, EMPTY);
             }
-            List<OgCereal> upperCaseQueryResultsList = ogCerealRepository.findByNameRegex(query);
-            List<OgCereal> lowerCaseQueryResultsList = ogCerealRepository.findByNameRegex(StringUtils.lowerCase(query));
-
-            // What if both results lists are not empty? Even with upper and lowercase regex of the
-            // same search term we may have a case that each lists contains necessary results that
-            // the other list does not contain
-            if (!upperCaseQueryResultsList.isEmpty() && !lowerCaseQueryResultsList.isEmpty()) {
-                maxQueryResultsList = joinShowcaseQueryResultsLists(upperCaseQueryResultsList, lowerCaseQueryResultsList);
-            } else {
-                maxQueryResultsList = upperCaseQueryResultsList.size() > lowerCaseQueryResultsList.size() ?
-                        upperCaseQueryResultsList : lowerCaseQueryResultsList;
-            }
-            model.addAttribute("ogFlakesList", maxQueryResultsList);
+            model.addAttribute(OG_FLAKES_LIST, maxQueryResultsList);
         } else {
-            // return all cereal options, even when user submits empty query - DEBUG
-            List<OgCereal> ogCerealRepositoryAllList = ogCerealRepository.findAll();
-            if (ogCerealRepositoryAllList.isEmpty() || allResultsAreNotValidated(ogCerealRepositoryAllList)) {
-                model.addAttribute("results", "empty");
-            }
-            model.addAttribute("ogFlakesList", ogCerealRepository.findAll());
+            // return all cereal options, when user submits empty query or page load
+            maxQueryResultsList = ogCerealService.getAllResults();
+            model.addAttribute(OG_FLAKES_LIST, maxQueryResultsList);
         }
 
         // add contributor message if contributor cookie exists
-        boolean isContributor = false;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("contributor") && cookie.getValue().equals("true")) {
-                    isContributor = true;
-                    break;
-                }
-            }
-        }
-        if (isContributor) {
+        if (ogCerealService.isContributor(request)) {
             // add dynamic text on successful upload on showcase redirect
-            StringBuilder message1 = new StringBuilder();
-            StringBuilder message2 = new StringBuilder();
-            message1.append("You've contributed to OGFlakes, no worries there's no limit.");
-            message2.append("Either way, we appreciate the love!");
-            model.addAttribute("message1", message1);
-            model.addAttribute("message2", message2);
+            StringBuilder message1 = new StringBuilder("You've contributed to OGFlakes, no worries there's no limit.");
+            StringBuilder message2 = new StringBuilder("Either way, we appreciate the love!");
+            model.addAttribute(MESSAGE_1, message1);
+            model.addAttribute(MESSAGE_2, message2);
         }
         return "showcase";
     }
@@ -106,16 +82,10 @@ public class OgCerealController {
 
     @PostMapping("/add")
     public String postOgCereal(@RequestParam("name") String name, @RequestParam("image") MultipartFile image,
-                                 @RequestParam("description") String description, HttpServletResponse response,
-                                 Model model) {
+                                 @RequestParam("description") String description, HttpServletResponse response) {
 
         ogCerealService.addOgCereal(name, description, image);
-
-        // add contributor cookie on successful cereal contribution
-        Cookie contributorCookie = new Cookie("contributor", "true");
-        contributorCookie.setMaxAge(7 * 24 * 60 * 60); // expire 7 days or until manual deletion
-        contributorCookie.setPath("/");
-        response.addCookie(contributorCookie);
+        ogCerealService.addContributorCookie(response);
         return "redirect:/ogcereal/showcase";
     }
 
@@ -128,31 +98,5 @@ public class OgCerealController {
             ogCerealRepository.save(ogCereal);
         }
         return "redirect:/ogcereal/showcase";
-    }
-
-    private List<OgCereal> joinShowcaseQueryResultsLists(List<OgCereal> resultsList1, List<OgCereal>resultsList2) {
-        // Do not add duplicates!
-        Set<OgCereal> queryResultsSet = new HashSet<>(ListUtils.union(resultsList1, resultsList2));
-        return new ArrayList<>(queryResultsSet);
-    }
-
-    /**
-     * If, minimum, 1 validated photo exists we return false because thymeleaf will act
-     * to render that one result on showcase page. We are checking if all results in the
-     * list are not validated. If we find that ALL results are not validated than we can
-     * communicate to the user that there are no results for its search.
-     *
-     * @param results {@link List<OgCereal>} results from repository search.
-     * @return boolean
-     */
-    private boolean allResultsAreNotValidated(List<OgCereal> results) {
-        boolean allNotValidated = true;
-        for (OgCereal cereal : results) {
-            if (cereal.getIsValidated()) {
-                allNotValidated = false;
-                break;
-            }
-        }
-        return allNotValidated;
     }
 }
--- src/main/java/com/morris/ogflakes/service/OgCerealService.java
+++ src/main/java/com/morris/ogflakes/service/OgCerealService.java
@@ -2,25 +2,40 @@ package com.morris.ogflakes.service;
 
 import com.morris.ogflakes.model.OgCereal;
 import com.morris.ogflakes.repository.OgCerealRepository;
+import org.apache.commons.collections4.ListUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.BsonBinarySubType;
 import org.bson.types.Binary;
-import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class OgCerealService  {
     private static final Logger logger = LoggerFactory.getLogger(OgCerealService.class);
 
+    private static final String CONTRIBUTOR = "contributor";
+    private static final String TRUE = "true";
+    private static final String ROOT = "/";
+
     @Autowired
     private OgCerealRepository ogCerealRepository;
 
+    /**
+     * Add {@link OgCereal} object to repository with name, description, and image.
+     *
+     * @param name        name of {@link OgCereal}
+     * @param description {@link OgCereal} description
+     * @param file        {@link MultipartFile} image
+     */
     public void addOgCereal(String name, String description, MultipartFile file) {
         OgCereal ogCereal = new OgCereal(name, description);
         try {
@@ -31,7 +46,121 @@ public class OgCerealService  {
         ogCerealRepository.insert(ogCereal);
     }
 
+    /**
+     * A case optimized query, queries the repo ignoring case. In such cases where a client searches using an uppercase
+     * keyword, the query may not return optimized results and may skip over the lowercase keyword. To optimize for full
+     * results, the case optimized query searches the repo for the keyword's upper and lowercase forms. The result is a
+     * joined {@link List<OgCereal>} of unique objects.
+     *
+     * @param query {@link String} keyword/term for repo search
+     * @return {@link List<OgCereal>}
+     */
+    public List<OgCereal> getCaseOptimizedResultsList(String query) {
+        List<OgCereal> maxQueryResultsList;
+        if (StringUtils.isAllLowerCase(query.substring(0,1))) {
+            query = StringUtils.join(query.substring(0,1).toUpperCase(), query.substring(1));
+        }
+        List<OgCereal> upperCaseQueryResultsList = ogCerealRepository.findByNameRegex(query);
+        List<OgCereal> lowerCaseQueryResultsList = ogCerealRepository.findByNameRegex(StringUtils.lowerCase(query));
+
+        if (!upperCaseQueryResultsList.isEmpty() && !lowerCaseQueryResultsList.isEmpty()) {
+            maxQueryResultsList = joinShowcaseQueryResultsLists(upperCaseQueryResultsList, lowerCaseQueryResultsList);
+        } else {
+            maxQueryResultsList = upperCaseQueryResultsList.size() > lowerCaseQueryResultsList.size() ?
+                    upperCaseQueryResultsList : lowerCaseQueryResultsList;
+        }
+        return maxQueryResultsList;
+    }
+
+    /**
+     * Adds contributor cookie to client response.
+     *
+     * @param response {@link HttpServletResponse}
+     */
+    public void addContributorCookie(HttpServletResponse response) {
+        Cookie contributorCookie = new Cookie(CONTRIBUTOR, TRUE);
+        contributorCookie.setMaxAge(7 * 24 * 60 * 60); // expire 7 days or until manual deletion
+        contributorCookie.setPath(ROOT);
+        response.addCookie(contributorCookie);
+    }
+
+    /**
+     * Determines if {@link List<OgCereal>} objects is empty or contains all
+     * non-validated {@link OgCereal} objects in list.
+     *
+     * @param results {@link List<OgCereal>} objects
+     * @return boolean
+     */
+    public boolean isEmptyOrNonValidatedResults(List<OgCereal> results) {
+        return (results.isEmpty() || !allResultsAreValidated(results));
+    }
+
+    /**
+     * Get all (validated) results from repo.
+     *
+     * @return {@link List<OgCereal>} validated object results
+     */
+    public List<OgCereal> getAllResults() {
+        return ogCerealRepository.findAll();
+    }
+
+    /**
+     * Determines weather client who sent {@link HttpServletRequest}has
+     * contributed to showcase by checking if contributor cookie exists.
+     *
+     * @param request {@link HttpServletRequest}
+     * @return boolean
+     */
+    public boolean isContributor(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(CONTRIBUTOR) && cookie.getValue().equals(TRUE)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get {@link Optional<OgCereal>} object from repo.
+     *
+     * @param id {@link OgCereal} id
+     * @return {@link Optional<OgCereal>} object.
+     */
     public Optional<OgCereal> getOgCereal(String id) {
         return ogCerealRepository.findById(id);
+    }
+
+    /**
+     * Joins the results of two {@link List<OgCereal>}.
+     *
+     * @param resultsList1 {@link List<OgCereal>} list 1.
+     * @param resultsList2 {@link List<OgCereal>} list 2.
+     * @return joined {@link List<OgCereal>} list.
+     */
+    private List<OgCereal> joinShowcaseQueryResultsLists(List<OgCereal> resultsList1, List<OgCereal>resultsList2) {
+        // Do not add duplicates!
+        Set<OgCereal> queryResultsSet = new HashSet<>(ListUtils.union(resultsList1, resultsList2));
+        return new ArrayList<>(queryResultsSet);
+    }
+
+    /**
+     * Determines if all results from query are validated. Helps determine which
+     * results are rendered to showcase. Showcase will not render non-validated
+     * results from repo.
+     *
+     * @param results {@link List<OgCereal>} results from repository search.
+     * @return boolean
+     */
+    private boolean allResultsAreValidated(List<OgCereal> results) {
+        for (OgCereal cereal : results) {
+            if (!cereal.getIsValidated()) {
+                return false;
+            }
+        }
+        logger.info("All containing results from queries are not validated: {}", results);
+        return true;
     }
 }
